0d108448a4660497c854f08369b34c2b8c54b5df
Afolabi Sobayo <sobayoa@gmail.com>
2021-03-15 07:40:40 +0100

    blog section

--- src/components/Learning.tsx
+++ src/components/Learning.tsx
@@ -3,10 +3,44 @@ import "aos/dist/aos.css";
 import AOS from "aos";
 import { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-
+import Slider from "react-slick";
 import { LandingHeader } from "./Common/LandingHeader";
 import { Title } from "./Common/Title";
 
+const sliderlandingConfig = {
+  dots: false,
+  arrows: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
 export const Learning = () => {
   useEffect(() => {
     AOS.init();
@@ -17,17 +51,42 @@ export const Learning = () => {
       <Title title="Blog" />
       <LandingHeader />
       <main role="main">
-        <section className="mt-5">
+        <section className="bg-pale">
           <Container>
             <Row>
-              <h1>Section One</h1>
+              <Col lg="12">
+              <Slider className="blog-slide" {...sliderlandingConfig}>
+              <div>
+                <div className="slide">
+                  <img src="/assets/images/img-slideblog-1.jpg" />
+                </div>
+              </div>
+              <div>
+                <div className="slide">
+                <img src="/assets/images/img-slideblog-1.jpg" />
+                </div>
+              </div>
+              <div>
+                <div className="slide">
+                <img src="/assets/images/img-slideblog-1.jpg" />
+                </div>
+              </div>
+            </Slider>
+              </Col>
             </Row>
-            <Row className="justify-content-center mt-3">
+
+            <Row className="justify-content-center mt-4">
               <Col>
-                <h2>Subsection 2a</h2>
+              <img src="/assets/images/img-blog-sec-1.jpg" />
               </Col>
               <Col>
-                <h2>Subsection 2b</h2>
+              <img src="/assets/images/img-blog-sec-2.jpg" />
+              </Col>
+              <Col>
+              <img src="/assets/images/img-blog-sec-3.jpg" />
+              </Col>
+              <Col>
+              <img src="/assets/images/img-blog-sec-4.jpg" />
               </Col>
             </Row>
           </Container>
